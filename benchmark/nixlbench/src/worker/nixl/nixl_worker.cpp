4351a1a66c281998a9f28649db30b59e6d624783
Ilia Yastrebov <iyastrebov@nvidia.com>
2026-06-15 19:12:52 +0200

    Nixlbench: register remote IOVs only for storage backends (#1772)

--- benchmark/nixlbench/src/worker/nixl/nixl_worker.cpp
+++ benchmark/nixlbench/src/worker/nixl/nixl_worker.cpp
@@ -1401,10 +1401,12 @@ registerIterationMem(nixlAgent *agent,
         return rc;
     }
 
-    nixl_reg_dlist_t remote_reg = iovListToNixlRegDlist(remote_iov, getRemoteSegType());
-    rc = agent->registerMem(remote_reg, &reg_args);
-    if (rc != NIXL_SUCCESS) {
-        return rc;
+    if (xferBenchConfig::isStorageBackend()) {
+        nixl_reg_dlist_t remote_reg = iovListToNixlRegDlist(remote_iov, getRemoteSegType());
+        rc = agent->registerMem(remote_reg, &reg_args);
+        if (rc != NIXL_SUCCESS) {
+            return rc;
+        }
     }
 
     return NIXL_SUCCESS;
@@ -1425,10 +1427,12 @@ deregisterIterationMem(nixlAgent *agent,
         return rc;
     }
 
-    nixl_reg_dlist_t remote_reg = iovListToNixlRegDlist(remote_iov, getRemoteSegType());
-    rc = agent->deregisterMem(remote_reg, &reg_args);
-    if (rc != NIXL_SUCCESS) {
-        return rc;
+    if (xferBenchConfig::isStorageBackend()) {
+        nixl_reg_dlist_t remote_reg = iovListToNixlRegDlist(remote_iov, getRemoteSegType());
+        rc = agent->deregisterMem(remote_reg, &reg_args);
+        if (rc != NIXL_SUCCESS) {
+            return rc;
+        }
     }
 
     return NIXL_SUCCESS;
